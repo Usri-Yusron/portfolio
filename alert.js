74609e2adff574157a146bd6fa196e690510e5dd
Muhamad Usri Yusron <121416729+Usri-Yusron@users.noreply.github.com>
2024-07-05 15:29:30 +0700

    Update alert.js
    matiin notifikasi alert, sama nambahin mansory

--- alert.js
+++ alert.js
@@ -1,1 +1,12 @@
-alert("untuk pengalaman yg lebih baik, gunakan handphone anda!")
+// alert("untuk pengalaman yg lebih baik, gunakan handphone anda!")
+
+// mansory
+document.addEventListener('DOMContentLoaded', function() {
+    var elem = document.querySelector('.grid');
+    var msnry = new Masonry(elem, {
+        // options
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-item',
+        percentPosition: true
+    });
+});
